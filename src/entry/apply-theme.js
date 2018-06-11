2386e56afc05a3ae7f7f57b6707c9e960bd62964
Mikhail <m.donsk@yandex.ru>
2018-06-11 18:34:49 +1000

    fix theme selection.

--- src/entry/apply-theme.js
+++ src/entry/apply-theme.js
@@ -1,5 +1,5 @@
 import options from '../options';
 
 const theme = options.getOption('theme').then(themeName => {
-  document.getElementsByTagName('html')[0].classList.add('theme-'+themeName);
+  document.getElementsByTagName('html')[0].setAttribute('class', 'theme-'+themeName);
 });
